440195bfb693dd2e6093c08e560070b9da06d2d9
kalakhi abdelmadjid <kalakhicheikhabdelmadjid@gmail.com>
2025-04-09 14:40:55 +0100

    after EXO1 TP0

---
+++ src/test/java/org/example/PersonTest.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+    Person A=new Person("kalakhi","madjid",24);
+    Person B=new Person("Aissani","Ramzi",17);
+    @Test
+    void getFullName() {
+        assertEquals("kalakhi madjid", A.getFullName());
+        assertEquals("Aissani Ramzi", B.getFullName());
+    }
+
+    @Test
+    void isAdult() {
+        assertEquals(true, A.isAdult());
+        assertEquals(false, B.isAdult());
+    }
+}
