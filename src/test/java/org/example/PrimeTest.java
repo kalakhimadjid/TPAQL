f3939eccc6750f62df8aa1bd58e2c695502bb358
kalakhi abdelmadjid <kalakhicheikhabdelmadjid@gmail.com>
2025-04-16 15:02:54 +0100

    after EXO 4 TP0

---
+++ src/test/java/org/example/PrimeTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeTest {
+
+    @Test
+    void isPrime() {
+        assertFalse(Prime.isPrime(-10));
+        assertFalse(Prime.isPrime(0));
+        assertTrue(Prime.isPrime(29));
+        assertTrue(Prime.isPrime(97));
+        assertFalse(Prime.isPrime(1));
+    }
+}
